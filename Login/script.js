5d360928a3a0627c2cb0f894a2e45e2dfd6d6024
Paulo Vitor <40708609+paulovitor20@users.noreply.github.com>
2025-02-09 11:18:25 -0300

    first commit

---
+++ Login/script.js
@@ -0,0 +1,14 @@
+function handleLogin(event) {
+    event.preventDefault(); // Evita o recarregamento da página
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "teste@teste.com" && password === "1234") {
+        alert("Login bem-sucedido!");
+        window.location.href = "dashboard.html"; // Redireciona após login
+    } else {
+        alert("Email ou senha inválidos!");
+    }
+}
+    
